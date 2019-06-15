54175898ca78d86b240c3a7e4fc6f2a5b5070252
Darío Alonso <dariovillar@hotmail.com>
2019-06-15 20:01:38 +0200

    Application keys

---
+++ AwesomeGitHub/AwesomeGitHub/Common/KeyValues.cs
@@ -0,0 +1,9 @@
+﻿namespace AwesomeGitHub
+{
+    public class KeyValues
+    {
+        public const string ApiBaseUrl = "https://api.github.com";
+        public static int MaxRepositories = 999;
+        public const string AppName = "AwesomeGithub";
+    }
+}
