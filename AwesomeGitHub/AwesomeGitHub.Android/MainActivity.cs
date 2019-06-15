b920988ae4568c031ff5e5a21e7680136a61dd09
Darío Alonso <dariovillar@hotmail.com>
2019-06-15 12:18:34 +0200

    Configuring Android Splash screen

--- AwesomeGitHub/AwesomeGitHub.Android/MainActivity.cs
+++ AwesomeGitHub/AwesomeGitHub.Android/MainActivity.cs
@@ -1,21 +1,20 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace AwesomeGitHub.Droid
 {
-    [Activity(Label = "AwesomeGitHub", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AwesomeGitHub", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            SetTheme(Resource.Style.MainTheme);
 
             base.OnCreate(savedInstanceState);
 
