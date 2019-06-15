13e2db751e8a081b725560d29b30656f3d0bbe64
Darío Alonso <dariovillar@hotmail.com>
2019-06-15 20:05:22 +0200

    Repository cell first layout

---
+++ AwesomeGitHub/AwesomeGitHub/Views/RepositoryViewCell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace AwesomeGitHub.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RepositoryViewCell
+    {
+        public RepositoryViewCell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            //UserImage.Source = null;
+
+            //if (ViewModel is null)
+            //    return;
+
+            //UserImage.Source = ViewModel.Owner.Avatar;
+            base.OnBindingContextChanged();
+        }
+    }
+}
