fcd735e3bb134479b373ce602bfd4f3eb3d70622
Darío Alonso <dariovillar@hotmail.com>
2019-06-16 10:53:56 +0200

    default language

--- AwesomeGitHub/AwesomeGitHub/Common/KeyValues.cs
+++ AwesomeGitHub/AwesomeGitHub/Common/KeyValues.cs
@@ -5,5 +5,6 @@
         public const string ApiBaseUrl = "https://api.github.com";
         public static int MaxRepositories = 999;
         public const string AppName = "AwesomeGithub";
+        public const string DefaultLanguage = "JavaScript";
     }
 }
