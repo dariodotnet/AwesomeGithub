cc0ac024cdb95fc902c82702a48f6e202f95bab5
Darío Alonso <dariovillar@hotmail.com>
2019-06-16 11:02:29 +0200

    Improve memory with cached images

--- AwesomeGitHub/AwesomeGitHub.Android/MainActivity.cs
+++ AwesomeGitHub/AwesomeGitHub.Android/MainActivity.cs
@@ -1,11 +1,13 @@
-﻿
-using Android.App;
-using Android.Content.PM;
-using Android.OS;
-using Android.Runtime;
-
-namespace AwesomeGitHub.Droid
+﻿namespace AwesomeGitHub.Droid
 {
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.OS;
+    using Android.Runtime;
+    using FFImageLoading;
+    using System;
+
     [Activity(Label = "AwesomeGitHub", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
@@ -24,11 +26,19 @@ namespace AwesomeGitHub.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnTrimMemory(TrimMemory level)
+        {
+            ImageService.Instance.InvalidateMemoryCache();
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            base.OnTrimMemory(level);
         }
     }
 }
