e00ab95347b62313be6030a0a915b37bd40ce677
Darío Alonso <dariovillar@hotmail.com>
2019-06-16 17:38:37 +0200

    navigating between pages

--- AwesomeGitHub/AwesomeGitHub/Views/HomeView.xaml.cs
+++ AwesomeGitHub/AwesomeGitHub/Views/HomeView.xaml.cs
@@ -41,6 +41,16 @@
                         GridAdder.TranslateTo(0, visible ? 0 : 60, 500);
                     });
 
+                this.WhenAnyValue(v => v.Repositories.SelectedItem)
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Where(x => x != null)
+                    .Subscribe(selected =>
+                    {
+                        ViewModel.SetCurrent((GitHubRepository)selected)
+                            .Subscribe(x => Navigation.PushAsync(new PullRequestView()));
+                    })
+                    .DisposeWith(d);
+
                 Observable.FromEventPattern<EventHandler<ItemVisibilityEventArgs>, ItemVisibilityEventArgs>(
                         x => Repositories.ItemAppearing += x,
                         x => Repositories.ItemAppearing -= x)
@@ -50,30 +60,22 @@
                     .Where(x => x)
                     .Select(x => Unit.Default)
                     .InvokeCommand(ViewModel.AddRepositoriesCommand).DisposeWith(d);
-
-                Observable.FromEventPattern<EventHandler<SelectedItemChangedEventArgs>, SelectedItemChangedEventArgs>(
-                        h => Repositories.ItemSelected += h,
-                        h => Repositories.ItemSelected -= h)
-                    .Where(x => x != null && x.EventArgs.SelectedItem != null)
-                    .Subscribe(selection =>
-                    {
-                        var repository = (GitHubRepository)selection.EventArgs.SelectedItem;
-                        Navigation.PushAsync(new PullRequestView(repository));
-
-                        (selection.Sender as ListView).SelectedItem = null;
-                    });
             });
             base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            Repositories.SelectedItem = null;
+            base.OnDisappearing();
         }
 
         private bool NeedLoad(int index)
         {
             var items = Repositories.ItemsSource.Cast<GitHubRepository>().Count();
-            Console.WriteLine($"TOTAL REPOSITORIES: {items}");
             if (items < 10 || items >= KeyValues.MaxRepositories)
                 return false;
 
-            Console.WriteLine($"COMPROBANDO INDICE: {index} : {items}");
             return index >= items - 5;
         }
     }
